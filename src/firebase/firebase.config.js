61c0bf5a522a4baefe4d97d4ae8a6b0d1fe9e3d8
didar <dider.alam115@gmail.com>
2021-10-16 22:55:26 +0600

    :construction: initial upload

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCz4pZpbuejum_lUCX5GyAck0vYHdAIMfk",
+	authDomain: "fir-authentication-2021-df1b6.firebaseapp.com",
+	projectId: "fir-authentication-2021-df1b6",
+	storageBucket: "fir-authentication-2021-df1b6.appspot.com",
+	messagingSenderId: "326293823077",
+	appId: "1:326293823077:web:d5a50f9683a0023fb3f08c",
+};
+
+
+export default firebaseConfig;
